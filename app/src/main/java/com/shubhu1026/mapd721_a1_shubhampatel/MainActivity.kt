ed7b145be5a60ec7aa1d4dd37f53ab34d85d2b00
shubhu1026 <shubhu.p2000@gmail.com>
2024-01-29 20:43:00 -0500

    Added 3 Text Fields

--- app/src/main/java/com/shubhu1026/mapd721_a1_shubhampatel/MainActivity.kt
+++ app/src/main/java/com/shubhu1026/mapd721_a1_shubhampatel/MainActivity.kt
@@ -3,11 +3,23 @@ package com.shubhu1026.mapd721_a1_shubhampatel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.shubhu1026.mapd721_a1_shubhampatel.ui.theme.MAPD721A1ShubhamPatelTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +31,56 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Text("Hello World")
+                    UserScreen()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun UserScreen(){
+    var username by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var id by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        // Name Text Field
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            value = username,
+            onValueChange = { username = it }
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Email Text Field
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            value = email,
+            onValueChange = { email = it }
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Id Text Field
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            value = id,
+            onValueChange = { id = it }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UserScreenPreview() {
+    MAPD721A1ShubhamPatelTheme {
+        UserScreen()
     }
 }
